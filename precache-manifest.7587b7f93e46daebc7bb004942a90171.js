1798af013289a16cf377e05e3210863b397bc249
sherry <1936839308@qq.com>
2020-08-10 10:01:18 +0800

    deploy

---
+++ precache-manifest.7587b7f93e46daebc7bb004942a90171.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f5dae4744fc917024e19a882bad3c85",
+    "url": "/money-react-website/index.html"
+  },
+  {
+    "revision": "166675bc694c8ce491ae",
+    "url": "/money-react-website/static/css/main.0278888b.chunk.css"
+  },
+  {
+    "revision": "7be211b96cfc6420767b",
+    "url": "/money-react-website/static/js/2.5c79a3da.chunk.js"
+  },
+  {
+    "revision": "768165ae6e4455b0f754a8444c7b06fa",
+    "url": "/money-react-website/static/js/2.5c79a3da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166675bc694c8ce491ae",
+    "url": "/money-react-website/static/js/main.3b062933.chunk.js"
+  },
+  {
+    "revision": "85563d754b0ec5d98e71",
+    "url": "/money-react-website/static/js/runtime-main.56c39c0f.js"
+  }
+]);
